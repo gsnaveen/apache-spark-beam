59707bc639d13351855f1895d3dbe3b9b5c89897
Naveen <gsnaveen@yahoo.com>
2018-03-12 11:05:39 -0700

    Create DataFramePivot.py

---
+++ DataFramePivot.py
@@ -0,0 +1,14 @@
+from pyspark.sql import functions as f
+df = spark.createDataFrame([("2018-01-01","userid1",10),("2018-01-09","userid2",20),("2018-01-17","userid3",33)], ['viewdate','userid','visits'])
+df = df.withColumn('weekofyear', f.weekofyear(df.viewdate))
+minWeek = df.selectExpr("min(weekofyear)").collect()
+
+df = df.withColumn('weekofyear', df.weekofyear - minWeek[0][0])
+StringAdd = 'Week_'
+df = df.withColumn("weekbucket", f.concat(f.lit(StringAdd),df.weekofyear.cast("string")))
+
+#--Pivot to get weeks as columns
+dfPivot = df.groupBy("userid").pivot("weekbucket").sum("visits")
+
+#--Filling NA with 0
+dfPivot = dfPivot.fillna(0)
