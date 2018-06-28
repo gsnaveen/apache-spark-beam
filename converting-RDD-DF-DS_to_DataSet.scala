04de92c9e888376ab91af1ef6447704d771a7318
Gantantar Naveen <gsnaveen@yahoo.com>
2018-06-28 13:00:44 -0700

    Update converting-RDD-DF-DS_to_DataSet.scala

--- converting-RDD-DF-DS_to_DataSet.scala
+++ converting-RDD-DF-DS_to_DataSet.scala
@@ -8,14 +8,17 @@ def myparser(x:String) : Option[dfSchema] = {
     val mypattern = x.split('\t');
     return Some(dfSchema(mypattern(0),mypattern(1),mypattern(2)))
   }
+import spark.implicits._
+val df = myRDD.flatMap(myparser).toDS()
 
-val df = myRDD.flatMap(myparser).toDF()
+
 //Reading as DataSet and converting it to a structured dataset
 // as[String is critical to have this as a data set]  
 var myDS = spark.read.text("/nav_sp1/*").as[String]
 //myDS: org.apache.spark.sql.Dataset[String] = [value: string]
 val df = myDS.flatMap(myparser)
 //res0: org.apache.spark.sql.Dataset[dfSchema] = [cookie: string, url: string ... 1 more field]
+
 
 //import as DataFrame myparser(x:Row)  x has to be rowType
 var myDF = spark.read.text("/nav_sp1/*")
