b0ec2ce215fefc286fe8a8b5536d68487222fcbc
Naveen <gsnaveen@yahoo.com>
2018-08-01 20:44:53 -0700

    Create ReadingWeblogIntoDataFrame.scala

---
+++ ReadingWeblogIntoDataFrame.scala
@@ -0,0 +1,25 @@
+import myfunctions._
+import org.apache.spark.sql.SparkSession
+/*
+import scala.io._
+import java.io._
+import java.util.Date
+import java.text.SimpleDateFormat
+import java.net.URLDecoder
+import java.util.UUID
+ */
+
+object webData {
+
+  def main(args: Array[String]) {
+    var spark = SparkSession.builder.appName("WeblogReading").master("local").getOrCreate(); //.enableHiveSupport()
+    spark.conf.set("spark.streaming.blockInterval", "20")
+    val sqlContext = spark.sqlContext
+    import sqlContext.implicits._
+    var myRDD = spark.read.text("file:///C:/tmp/da/mytestData.log").as[String] //textFile
+
+    val df = myRDD.flatMap(weblogparser).as[webrow]
+    df.show()
+    spark.stop()
+  }
+}
