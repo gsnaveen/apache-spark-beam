97d359a7c7339e27d5282c25737040cf3fd7df90
Naveen <gsnaveen@yahoo.com>
2018-09-22 15:16:39 -0700

    Update dataframe_skew.scala

--- dataframe_skew.scala
+++ dataframe_skew.scala
@@ -5,7 +5,17 @@ df1 = df1.withColumn("rowNum",row_number().over(Window.partitionBy(col("ip")).or
 var dfg = df1.groupBy(col("ip"),col("rowNum")).agg(sum(col("orders")).alias("sum1"))
 dfg = dfg.groupBy(col("ip")).agg(sum(col("sum1")).alias("TotalSum"))
 dfg.show()
+
+
+// Redistributing data based on columns 
+val repartDF = df1.repartition($"ip",$"rownum") 
+repartDF.rdd.partitions.size
+repartDF.write.csv("mypart1x")
+
+
 /*
+
+
 
 +---+--------+
 | ip|TotalSum|
