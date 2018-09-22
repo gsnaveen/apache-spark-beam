7c41b2ac590288152eaf14e721593f7408361505
Naveen <gsnaveen@yahoo.com>
2018-09-22 10:59:35 -0700

    Create dataframe_part_coal_sample.scala

---
+++ dataframe_part_coal_sample.scala
@@ -0,0 +1,10 @@
+//Writing to given number of files
+
+df.coalesce(5).write.csv("/app/db/folder")		# will create only 5 files which may not be equal in size.
+df1 = df1.coalesce(7)
+df1.rdd.partitions.size 
+
+val df1 = df1.repartition(5)
+colorDf = peopleDf.repartition($"color") //Creates 200 Partiontions some will be empty
+
+val df1Sample = df1.sample(true, 0.10).coalesce(1)
