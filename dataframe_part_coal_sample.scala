dd30f966159d15e412ea026ed5b3ef863801f7f2
Naveen <gsnaveen@yahoo.com>
2018-09-22 11:57:56 -0700

    Update dataframe_part_coal_sample.scala

--- dataframe_part_coal_sample.scala
+++ dataframe_part_coal_sample.scala
@@ -5,6 +5,6 @@ df1 = df1.coalesce(7)
 df1.rdd.partitions.size 
 
 val df1 = df1.repartition(5)
-colorDf = peopleDf.repartition($"color") //Creates 200 Partiontions some will be empty
+locationDf = peopleDf.repartition($"location") //Creates 200 Partiontions some will be empty
 
 val df1Sample = df1.sample(true, 0.10).coalesce(1)
