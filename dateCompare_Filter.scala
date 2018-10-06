8f3e6183220b07f423b3617e8c2a45ba52f2bac6
Naveen <gsnaveen@yahoo.com>
2018-10-06 14:58:56 -0700

    Update dateCompare_Filter.scala

--- dateCompare_Filter.scala
+++ dateCompare_Filter.scala
@@ -17,12 +17,18 @@ object datetest2 {
 
     val innowDate: String = "2018-08-04"
     val formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
-    val gtDate = formatter.parseDateTime(innowDate).minusDays(2).toString("yyy-MM-dd") //56 days for our scenario
+    val gtDate = formatter.parseDateTime(innowDate).minusDays(2).toString("yyy-MM-dd") //56 days for the customer scenario
 
     val df = spark.read.option("header","true").option("sep","\t").csv("inData.tsv")
-    val dfAns = df.filter(col("data_date").cast("date").cast("String").gt(gtDate)).groupBy("custid").agg(sum(col("buy")).alias("total"),(sum(col("buy"))/8).alias("avg"))
+    val dfAns = df.filter(col("data_date").gt(gtDate)).groupBy("custid").agg(sum(col("buy")).alias("total"),(sum(col("buy"))/8).alias("avg")) //takes into consideration the time component of the date as well
+    val dfAns2 = df.filter(col("data_date").cast("date").cast("String").gt(gtDate)).groupBy("custid").agg(sum(col("buy")).alias("total"),(sum(col("buy"))/8).alias("avg")) // this takes only the date into consideration
 
+    println("Date only greater than")
     dfAns.show()
+    
+    println("Date time gireater than")
+    dfAns2.show()
+
     println(dfAns.printSchema())
     dfAns.coalesce(1).write.mode("overwrite").option("header","true").csv("myoutput.csv")
     spark.stop()
