b4ab27a363dee780907c5f824e63733c58e2218d
Naveen <gsnaveen@yahoo.com>
2019-02-19 12:16:03 -0800

    Update spark_includes.scala

--- spark_includes.scala
+++ spark_includes.scala
@@ -1,4 +1,13 @@
 import org.apache.spark.sql.{Row,SaveMode, SparkSession}
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions._ //{col, lit, to_timestamp, udf,concat,round}
 import org.apache.spark.sql.expressions._ //Window
+import org.apache.spark.sql.types.{StringType,DoubleType,DateType}
+
 import org.joda.time.format.DateTimeFormat
+import org.joda.time.{DateTime, Duration,Period}
+
+
+import java.util.Properties
+  val connectionProperties = new Properties()
+  connectionProperties.put("user", s"${jdbcUsername}")
+  connectionProperties.put("password", s"${jdbcPassword}")
