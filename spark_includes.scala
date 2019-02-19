bade06d7c7c09ed1c1e34667da7f1826075bd131
Naveen <gsnaveen@yahoo.com>
2019-02-19 12:22:05 -0800

    Update spark_includes.scala

--- spark_includes.scala
+++ spark_includes.scala
@@ -1,13 +1,34 @@
+//Spark
 import org.apache.spark.sql.{Row,SaveMode, SparkSession}
 import org.apache.spark.sql.functions._ //{col, lit, to_timestamp, udf,concat,round}
 import org.apache.spark.sql.expressions._ //Window
 import org.apache.spark.sql.types.{StringType,DoubleType,DateType}
 
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.storage.StorageLevel
+
+//DateTime
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.{DateTime, Duration,Period}
 
+//Java
+import java.util.regex.Pattern
+import java.util.regex.Matcher
 
 import java.util.Properties
   val connectionProperties = new Properties()
   connectionProperties.put("user", s"${jdbcUsername}")
   connectionProperties.put("password", s"${jdbcPassword}")
+
+ df.write
+      .format("org.elasticsearch.spark.sql")
+
+val reader = spark.read
+      .format("org.elasticsearch.spark.sql")
+
+val df = spark.read
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map( "table" -> "words_new", "keyspace" -> "test_keyspace"))
+
+
