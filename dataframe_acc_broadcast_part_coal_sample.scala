7bffc8a4ca026dbc645a337e232377cffe6bc68c
Naveen <gsnaveen@yahoo.com>
2019-02-18 16:11:36 -0800

    Update and rename dataframe_acc_part_coal_sample.scala to dataframe_acc_broadcast_part_coal_sample.scala

--- dataframe_acc_broadcast_part_coal_sample.scala
+++ dataframe_acc_broadcast_part_coal_sample.scala
@@ -1,3 +1,10 @@
+
+val counter = sc.longAccumulator("counter")
+counter.add(10)
+
+val df22 = spark.read.option("sep", "\t").option("header","true").csv("inData2.txt")
+val df2 = broadcast(df22)
+
 //Writing to given number of files
 
 df.coalesce(5).write.csv("/app/db/folder")		# will create only 5 files which may not be equal in size.
@@ -8,9 +15,6 @@ val df1 = df1.repartition(5)
 locationDf = peopleDf.repartition($"location") //Creates 200 Partiontions some will be empty
 
 val df1Sample = df1.sample(true, 0.10).coalesce(1)
-
-val counter = sc.longAccumulator("counter")
-counter.add(10)
 
 val columns = Seq("level1","level2")
 df1.write.partitionBy(columns:_*).mode("overwrite").save(s"$hive_warehouse/$dbname.db/$temp_table/")
