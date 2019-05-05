b857fe423b8ec991e707f1950ad7a2d0bea0f7fe
Naveen <gsnaveen@yahoo.com>
2019-05-04 17:15:24 -0700

    Create hive_env_parameters.sql

---
+++ hive_env_parameters.sql
@@ -0,0 +1,17 @@
+SET mapreduce.reduce.memory.mb=8192;
+SET mapreduce.reduce.java.opts=-Xmx6144m;
+SET mapreduce.map.memory.mb=1024;
+SET mapreduce.map.java.opts=-Xmx768m;
+--SET mapreduce.map.memory.mb=8192;
+--SET mapreduce.map.java.opts=-Xmx6144m;
+SET hive.execution.engine=mr; --tez,spark
+SET mapreduce.input.fileinputformat.split.minsize=512000000;
+set hive.optimize.ppd=true;
+set hive.optimize.ppd.storage=true;
+set hive.exec.compress.intermediate=true;
+set hive.exec.parallel=true;
+set hive.merge.mapfiles=true;
+set hive.merge.mapredfiles=true;
+set hive.merge.size.per.task=512000000;
+set hive.merge.smallfiles.avgsize=512000000;
+--set hive.smalltable.filesize=512000000;
