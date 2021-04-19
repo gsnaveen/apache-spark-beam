b9bfed404111abd1e22dc2e2d52af73f5ab874bb
Naveen <gsnaveen@yahoo.com>
2021-04-19 09:13:07 -0700

    Create hive_map_attribute.sql

---
+++ hive_map_attribute.sql
@@ -0,0 +1,22 @@
+
+create table db.my_testmap (
+name      string ,   
+ph        string ,   
+category  map<string,string>
+)
+
+Select * from db.my_testmap
+
+
+insert into db.my_testmap (name,ph,category) values('n1','p1',map("key1","value1","key2","value2"))
+
+insert into db.my_testmap (name,ph,category) values('n2','p2',map("key1","value2","key2","value22"))
+
+Select * from db.my_testmap
+
+Select name,ph,category['key1'],category['key2'] from db.my_testmap
+
+Presto:
+
+Select name,ph,category['key1'],category['key2'] 
+from hive_stg.db.my_testmap
