fc35498e4f24f747a122dce7f5a8d0099c14595e
Naveen <gsnaveen@yahoo.com>
2022-11-16 14:45:17 -0800

    Update pySpark_udf.py

--- pySpark_udf.py
+++ pySpark_udf.py
@@ -13,3 +13,13 @@ def getDomain(value):
 udfgetDomain = udf(getDomain, StringType())
 sess_with_domain = sess.withColumn("RequestDomain", udfgetDomain("url"))
 sess_with_domain.show()
+
+# Another example
+def mystructcall (val1,val2):
+    mystruct = {"refreshopp":None,"servicerenewal":None}
+    mystruct["refreshopp"] = val1
+    mystruct["servicerenewal"] = val2
+    return mystruct
+	
+mystructcall = udf(mystructcall, StructType().add("refreshopp", IntegerType(), True).add("servicerenewal", IntegerType(), True, None))
+df_with_struct = df.withColumn("mystruct1", mystructcall("refreshopp","servicerenewal"))
